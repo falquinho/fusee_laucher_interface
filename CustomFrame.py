b6891a27ea208ee8cb1c30b6f99b53a1d9b6b27e
falquinho <lucas.falcao.nb@gmail.com>
2018-06-01 23:30:32 -0300

    Space animation displays correctly.

--- CustomFrame.py
+++ CustomFrame.py
@@ -1,4 +1,5 @@
 import wx
+import wx.adv
 
 
 
@@ -14,16 +15,20 @@ class CustomFrame(wx.Frame):
 
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
         self.fileSizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.spaceAnim = wx.adv.AnimationCtrl(self, size=wx.Size(200, 200), style=wx.adv.AC_NO_AUTORESIZE)
         self.devLabel  = wx.StaticText(self, label="Looking for device")
         self.btnOpen   = wx.Button(self, -1, "Select Payload")
         self.fileLabel = wx.StaticText(self, label="No payload selected.")
         self.btnLaunch = wx.Button(self, -1, "Launch Fusée!")
 
+        self.spaceAnim.LoadFile("space_anim.gif")
+
         self.fileSizer.Add(self.btnOpen, 0, wx.ALL|wx.ALIGN_LEFT, 8)
         self.fileSizer.Add(self.fileLabel, 0, wx.ALL|wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL, 8)
 
+        self.mainSizer.Add(self.spaceAnim, 0, wx.ALL|wx.ALIGN_CENTER, 8)
         self.mainSizer.Add(self.devLabel, 0, wx.ALL|wx.ALIGN_CENTER, 8)
-        self.mainSizer.Add(self.fileSizer, 0, wx.ALL, 8)
+        self.mainSizer.Add(self.fileSizer, 0, wx.ALL, 0)
         self.mainSizer.Add(self.btnLaunch, 0, wx.ALL|wx.EXPAND, 8)
 
         self.SetSizerAndFit(self.mainSizer)
@@ -31,6 +36,8 @@ class CustomFrame(wx.Frame):
         self.validateLaunch()
 
         self.Layout()
+
+        self.spaceAnim.Play()
 
         self.Bind(wx.EVT_TIMER, self.onTimerUpdate)
         self.myTimer.Start(300)
