c398ef29dd28c74ffcd50b14acef7ecc9fe313a3
falquinho <lucas.falcao.nb@gmail.com>
2018-06-03 19:25:39 -0300

    Added rocket animation.

--- CustomFrame.py
+++ CustomFrame.py
@@ -22,18 +22,24 @@ class CustomFrame(wx.Frame):
 
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
         self.fileSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.spaceAnim = wx.adv.AnimationCtrl(self, size=wx.Size(200, 200), style=wx.adv.AC_NO_AUTORESIZE)
+        self.animPanel = wx.Panel(self, size=wx.Size(200, 200))
+        self.spaceAnim = wx.adv.AnimationCtrl(self.animPanel, size=wx.Size(200, 200), style=wx.adv.AC_NO_AUTORESIZE)
+        self.rocktAnim = wx.adv.AnimationCtrl(self.animPanel, size=wx.Size(200, 200), style=wx.adv.AC_NO_AUTORESIZE)
         self.devLabel  = wx.StaticText(self, label="Looking for device")
         self.btnOpen   = wx.Button(self, -1, "Select Payload")
         self.fileLabel = wx.StaticText(self, label="No payload selected.")
         self.btnLaunch = wx.Button(self, -1, "Launch Fusée!")
 
         self.spaceAnim.LoadFile("space_anim.gif")
+        self.rocktAnim.LoadFile("rocket_anim.gif")
+        self.spaceAnim.Play()
+        self.rocktAnim.Play()
+        self.rocktAnim.Hide()
 
         self.fileSizer.Add(self.btnOpen, 0, wx.ALL|wx.ALIGN_LEFT, 8)
         self.fileSizer.Add(self.fileLabel, 0, wx.RIGHT|wx.ALIGN_LEFT|wx.ALIGN_CENTER_VERTICAL, 8)
 
-        self.mainSizer.Add(self.spaceAnim, 0, wx.ALL|wx.ALIGN_CENTER, 8)
+        self.mainSizer.Add(self.animPanel, 0, wx.ALL|wx.ALIGN_CENTER, 8)
         self.mainSizer.Add(self.devLabel, 0, wx.ALL|wx.ALIGN_CENTER, 8)
         self.mainSizer.Add(self.fileSizer, 0, wx.ALL, 0)
         self.mainSizer.Add(self.btnLaunch, 0, wx.ALL|wx.EXPAND, 8)
@@ -44,7 +50,6 @@ class CustomFrame(wx.Frame):
 
         self.Layout()
 
-        self.spaceAnim.Play()
 
         self.Bind(wx.EVT_BUTTON, self.onBtnOpenPressed, self.btnOpen)
         self.Bind(wx.EVT_BUTTON, self.onBtnLaunchPressed, self.btnLaunch)
@@ -66,11 +71,14 @@ class CustomFrame(wx.Frame):
         dev = self.haxBackend.find_device(self.devVid, self.devPid)
         if dev != None:
             self.devConnected = True
+            self.rocktAnim.Show()
             self.devLabel.SetLabel("Device found!")
         else:
             self.devConnected = False
+            self.rocktAnim.Hide()
             self.devLabel.SetLabel("Looking for device"+("."*self.noTicks))
             self.noTicks = (self.noTicks+1)%4
+
         self.validateLaunch()
 
 
